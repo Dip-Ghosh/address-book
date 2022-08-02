9f448e6efd58170e13f579656588213c28b07e8d
dip.ghosh <dip.ghosh@shohoz.com>
2022-08-02 13:53:18 +0600

    alignment change

--- config.php
+++ config.php
@@ -7,8 +7,6 @@ $dbname     = "address-book";
 
 $conn       = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-
+if ( $conn->connect_error ) {
     die("Connection failed: " . $conn->connect_error);
-
 }
